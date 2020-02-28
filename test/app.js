2a630d6ed04f7e0c840306ca3a01d0d2c749f9af
rongzedong <rongzedong@users.noreply.github.com>
2020-02-28 23:41:53 +0800

    Update app.js

--- test/app.js
+++ test/app.js
@@ -2,15 +2,15 @@
  * Created by wandergis on 15/11/17.
  */
 //国测局坐标(火星坐标,比如高德地图在用),百度坐标,wgs84坐标(谷歌国外以及绝大部分国外在线地图使用的坐标)
-var coordtransform = require('../index');
+var coordtransform = require('../GPSTrans');
 //百度经纬度坐标转国测局坐标
-var bd09togcj02 = coordtransform.bd09togcj02(116.404, 39.915);
+var bd09togcj02 = GPSTrans.bd09togcj02(116.404, 39.915);
 //国测局坐标转百度经纬度坐标
-var gcj02tobd09 = coordtransform.gcj02tobd09(116.404, 39.915);
+var gcj02tobd09 = GPSTrans.gcj02tobd09(116.404, 39.915);
 //wgs84转国测局坐标
-var wgs84togcj02 = coordtransform.wgs84togcj02(116.404, 39.915);
+var wgs84togcj02 = GPSTrans.wgs84togcj02(116.404, 39.915);
 //国测局坐标转wgs84坐标
-var gcj02towgs84 = coordtransform.gcj02towgs84(116.404, 39.915);
+var gcj02towgs84 = GPSTrans.gcj02towgs84(116.404, 39.915);
 console.log(bd09togcj02);
 console.log(gcj02tobd09);
 console.log(wgs84togcj02);
